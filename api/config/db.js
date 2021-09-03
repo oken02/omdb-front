a3954b5191ae0e77fab0abe2aa0123da4bbc81ad
kevin <kevin.c.quiroz>
2021-09-03 18:37:12 -0500

    last commit

--- api/config/db.js
+++ api/config/db.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require("sequelize");
 
 // Option 2: Passing parameters separately (other dialects)
-const db = new Sequelize("omdb", "oken02", "nose123", {
-  logging:false,
+
+const db = new Sequelize("my_omdb", null, null, {
+  logging: false,
   host: "localhost",
   dialect: "postgres" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
 });
